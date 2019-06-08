cbd863cd287341e33cc042259df839aa0c1e6af6
fsorodrigues <desouzaoliveirarod.f@husky.neu.edu>
2019-06-07 21:12:47 -0400

    init pop

---
+++ batch-ocr.sh
@@ -0,0 +1,9 @@
+for file in ./raw-pdfs/*
+do
+    pathIn="$(basename $file)"
+    pathOut="$(echo $pathIn | tr '\n' ' ' | sed -e 's/[[:space:]]*$//')"
+    basepath="./ocr-pdfs"
+    outputFile="$basepath/$pathOut"
+    echo $outputFile
+    ocrmypdf --output-type pdfa "$file" "$outputFile" --force-ocr
+done
